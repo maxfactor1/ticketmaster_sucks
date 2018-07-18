45c7862361546b4349d6c54dd25b3d22fdaa3f0c
Max Buehler <max.buehler@nucor.com>
2018-07-18 10:07:30 -0500

    serious commit message

--- test.py
+++ test.py
@@ -1,32 +1,45 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver 
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC 
 from selenium.common.exceptions import TimeoutException
+import filecmp
 
 option = webdriver.ChromeOptions()
-option.add_argument(" - incognito")
+#option.add_argument(" - incognito")
+option.add_argument('headless')
 
+#print(option)
 browser = webdriver.Chrome(executable_path='/Users/mbuehl01/Downloads/chromedriver', chrome_options=option)
 
 browser.get("http://settlement.livenation.com/")
 
-# Wait 3 seconds for page to load
-timeout = 3
+# Wait 20 seconds for page to load
+timeout = 10
 try:
-    WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//img[@class='howTo-9b7f69e4528bc57d25cb1f7ee1996124']")))
+    WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//*[starts-with(@class, 'LNFooter-')]")))
 except TimeoutException:
     print("Timed out waiting for page to load")
     browser.quit()
+    quit()
 
-# find_elements_by_xpath returns an array of selenium objects.
-# events_element = browser.find_elements_by_xpath("//[@class='eventsSection-be329ef68095bbb58c8729d2b4abf0a6']")
-events_element = browser.find_elements_by_xpath("//[starts-with(@class, 'eventsSection-'])")
+events_element = browser.find_elements_by_xpath("//*[starts-with(@class, 'eventsSection-')]")
 
-# use list comprehension to get the actual repo titles and not the selenium objects.
 events = [x.text for x in events_element]
-# print out all the titles.
-print('events:')
-print(events, '\n')
+f1=open("/tmp/before", "rw")
+f2=open("/tmp/after", "rw")
+
+for event in events:
+	print event.encode('utf-8').strip()
+
+before=f1.read()
+after=f2.read()
+
+#if before != after:
+#	for event.encode('utf-8').strip() in events:
+#	        f1.write("%s\n" % event)
+
+f1.close()
+f2.close()    
 
 browser.quit()
